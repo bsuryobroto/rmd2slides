c6380e13f6789b60841dd33899422ba415f6407d
bsuryobroto <bsuryobroto@gmail.com>
2024-12-21 10:44:15 +0700

    initiation commit..

---
+++ README.md
@@ -0,0 +1,7 @@
+# Reveal.js slides composed in Rmarkdown
+
+Check these two examples..
+  
+[2-Column](https://bsuryobroto.github.io/_revealjs/revealjs_2columns.html) slide is written in revealjs_2columns.Rmd..
+  
+[IPB logo in background](https://bsuryobroto.github.io/_revealjs/revealjs_background.html) is written in revealjs_background.Rmd..
---
+++ revealjs/render.R
@@ -0,0 +1,14 @@
+library(rmarkdown)
+# install.packages("revealjs", type = "source")
+library(revealjs)
+rmarkdown::render("revealjs_background.Rmd")
+
+#-----
+# github project
+# https://github.com/bsuryobroto/_revealjs
+#
+# rendered slides
+# https://bsuryobroto.github.io/_revealjs/revealjs_2columns.html
+#
+# https://bsuryobroto.github.io/_revealjs/revealjs_background.html
+#-----
